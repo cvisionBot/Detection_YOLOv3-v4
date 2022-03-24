e3318ae09f368daeaebe49fa734b3691030882a0
Jack Yoon <cvisionyoon@gmail.com>
2022-03-24 23:12:49 +0000

    create yolo_loss

--- models/loss/yolo_loss.py
+++ models/loss/yolo_loss.py
@@ -2,7 +2,6 @@ import math
 import torch
 
 from torch import nn
-# from models.detector.anchors import anchors
 
 def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, eps=1e-9):
     box2 = box2.T
@@ -50,5 +49,35 @@ def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, eps=
 
 
 class YOLO_Loss(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self, cfg, anchors):
         super(YOLO_Loss, self).__init__()
+        self.cfg = cfg
+        self.branch_anchors = anchors
+        self.num_anchors = len(self.branch_anchors) # 3
+        self.bbox_attrs = 5 + self.cfg['classes']
+        self.img_w = self.cfg['input_size']
+        self.img_h = self.cfg['input_size']
+
+        self.ignore_threshold = 0.5
+        self.lambda_xy = 0.5
+        self.lambda_wh = 2.5
+        self.lambda_conf = 1.0
+        self.lambda_cls = 1.0
+
+        self.mse_loss = nn.MSELoss()
+        self.bce_loss = nn.BCELoss()
+
+    def forward(self, input):
+        pred, target = input
+        batch_size, _, layer_h, layer_w = pred.size()
+        stride_h = self.img_h / layer_h
+        stride_w = self.img_w / layer_w
+        scaled_anchors = [(a_w / stride_w, a_h / stride_h) for a_w, a_h in self.branch_anchors]
+
+        # [b, 3, (5 + 20), layer_h, layer_w]
+        prediction = pred.view(batch_size, self.num_anchors, self.bbox_attrs, layer_h, layer_w).permute(0, 1, 3, 4, 2).contiguous()
+        
+
+        
+
+
--- module/detector.py
+++ module/detector.py
@@ -3,18 +3,22 @@ import pytorch_lightning as pl
 from torch import nn
 from utils.module_select import get_optimizer
 from models.loss.yolo_loss import YOLO_Loss
+from models.detector.anchors import pascal_voc, coco
 
 class Detector(pl.LightningModule):
     def __init__(self, model, cfg, epoch_length=None):
         super(Detector, self).__init__()
         self.save_hyperparameters(ignore='model')
         self.model = model
+        self.branch1_anchors = pascal_voc['anchors'][0]
+        self.branch2_anchors = pascal_voc['anchors'][1]
+        self.branch3_anchors = pascal_voc['anchors'][2]
         self.loss_fn = YOLO_Loss(cfg)
 
 
     def forward(self, input):
-        output1, output2, output3 = self.model(input)
-        return output1, output2, output3
+        branch1, branch2, branch3 = self.model(input)
+        return branch1, branch2, branch3
 
 
     def training_step(self, batch, batch_idx):
@@ -23,8 +27,9 @@ class Detector(pl.LightningModule):
 
 
     def common_opt_training_step(self, batch):
-        output1, output2, output3 = self.model(batch['img'])
-        loss = self.loss_fn([output1, output2, output3])
+        branch1, branch2, branch3 = self.model(batch['img'])
+        branch1_loss= self.loss_fn
+        loss = self.loss_fn([branch1, branch2, branch3, batch])
         return loss
 
 
